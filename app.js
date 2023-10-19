e3fd39f72304f2f24665b7c61fe78ac2c3f611a2
scolorado <224scolorado@frshd.com>
2023-10-19 09:59:37 -0400

    fixing errors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const square = document.querySelectorAll('.square')
-const mole = document.querySelectorAll('.mole')
-const timeLeft = document.querySelectorAll('#time-left')
-let score = document.querySelector('#score')
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
 
 let result = 0
 let hitPosition
@@ -9,38 +9,42 @@ let currentTime = 60
 let timerId = null
 
 function randomSquare() {
-    square.forEach(className => {
-        className.classList.remove('mole')
-    })
-    let randomPosition = square [Math.floor(Math.random()*9)]
-    randomPosition.classList.add('mole')
+  squares.forEach(square => {
+    square.classList.remove('mole')
+  })
 
-    hitPosition = randomPosition.id
+  let randomSquare = squares[Math.floor(Math.random() * 9)]
+  randomSquare.classList.add('mole')
+
+  hitPosition = randomSquare.id
 }
 
-square.forEach (id => {
-    id.addEventListener('mouseup', () => {
-        if (if.id === hitPosition){
-            result = result +1
-            score.textContent = result
-        }
-    })
+squares.forEach(square => {
+  square.addEventListener('mousedown', () => {
+    if (square.id == hitPosition) {
+      result++
+      score.textContent = result
+      hitPosition = null
+    }
+  })
 })
 
-
-function moveMole(){
-    let timerId = null
-    timerId = setInterval (randomSquare, 1000)
+function moveMole() {
+  timerId = setInterval(randomSquare, 500)
 }
+
+moveMole()
 
 function countDown() {
-    currentTime --
-    timeLeft.textContent = currentTime
+ currentTime--
+ timeLeft.textContent = currentTime
 
-    if (currentTime === 0) {
-        clearInterval (timerId)
-        alert ('GAME OVER! Your final score is' + result)
-    }
+ if (currentTime == 0) {
+   clearInterval(countDownTimerId)
+   clearInterval(timerId)
+   alert('GAME OVER! Your final score is ' + result)
+ }
+
 }
 
-let timerId = setInterval (countDown, 1000)
+let countDownTimerId = setInterval(countDown, 1000)
